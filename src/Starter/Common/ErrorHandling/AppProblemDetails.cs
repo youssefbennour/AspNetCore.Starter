abc922a6a86525211cb1a411a93f0b986e985837
youssefbennour <youssefbennour15@gmail.com>
2024-04-22 12:14:40 +0100

    REFACTOR: add default value for AppProblemDetails' Message

--- src/Starter/Common/ErrorHandling/AppProblemDetails.cs
+++ src/Starter/Common/ErrorHandling/AppProblemDetails.cs
@@ -4,7 +4,8 @@ using System.Text.Json.Serialization;
 
 namespace Starter.Common.ErrorHandling {
     public class AppProblemDetails : ProblemDetails {
-        public string Message { get; set; }
+        public string Message { get; set; } = default!;
+
         [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingNull)]
         public List<FieldValidationError>? Errors { get; set; }
     }
