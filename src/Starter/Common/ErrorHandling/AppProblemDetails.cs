ab54a7b5e7cd76039689cf4f71902a2dbdcafcf4
youssefbennour <youssefbennour15@gmail.com>
2024-04-22 11:55:19 +0100

    FEAT: extend Microsoft's 'ProblemDetails' object

---
+++ src/Starter/Common/ErrorHandling/AppProblemDetails.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Starter.Common.ErrorHandling.ErrorModels;
+using System.Text.Json.Serialization;
+
+namespace Starter.Common.ErrorHandling {
+    public class AppProblemDetails : ProblemDetails {
+        public string Message { get; set; }
+        [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingNull)]
+        public List<FieldValidationError>? Errors { get; set; }
+    }
+}
