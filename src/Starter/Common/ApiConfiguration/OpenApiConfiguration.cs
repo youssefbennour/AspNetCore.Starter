f6cec66b9416690cedc67b8b1966e83bf0084b1d
youssefbennour <youssefbennour15@gmail.com>
2024-04-25 11:11:02 +0100

    Feat: add Accept-Language header parameter for swagger docs

--- src/Starter/Common/ApiConfiguration/OpenApiConfiguration.cs
+++ src/Starter/Common/ApiConfiguration/OpenApiConfiguration.cs
@@ -1,46 +1,26 @@
-﻿using NSwag;
-using NSwag.Generation.Processors;
-using NSwag.Generation.Processors.Contexts;
+﻿using Microsoft.OpenApi.Models;
+using Swashbuckle.AspNetCore.SwaggerGen;
 
-namespace Starter.Common.ApiConfiguration
-{
-    internal static class OpenApiConfiguration
-    {
-        internal static IServiceCollection AddOpenApiConfiguration(this IServiceCollection services)
-        {
-            services.AddOpenApiDocument(opts =>
-            {
-                opts.Title = "API name";
-                opts.OperationProcessors.Add(new AcceptLanguageHeaderParameter());
+namespace Starter.Common.ApiConfiguration {
+    internal static class OpenApiConfiguration {
+        internal static IServiceCollection AddOpenApiConfiguration(this IServiceCollection services) {
+            services.AddSwaggerGen(c => {
+                c.OperationFilter<AcceptLanguageHeaderParameter>();
             });
 
             return services;
         }
     }
 
-    internal class AcceptLanguageHeaderParameter : IOperationProcessor
-    {
-        public bool Process(OperationProcessorContext context)
-        {
-            var parameter = new OpenApiParameter
-            {
+    internal class AcceptLanguageHeaderParameter : IOperationFilter {
+        public void Apply(OpenApiOperation operation, OperationFilterContext context) {
+            operation.Parameters.Add(new OpenApiParameter() {
                 Name = "Accept-Language",
-                Kind = OpenApiParameterKind.Header,
+                In = ParameterLocation.Header,
+                Required = false,
                 Description = "Language preference for the response.",
-                IsRequired = true,
-                IsNullableRaw = true,
-                Default = "en-US",
-                Schema = new NJsonSchema.JsonSchema()
-                {
-                    Type = NJsonSchema.JsonObjectType.String,
-                    Item = new NJsonSchema.JsonSchema() { Type = NJsonSchema.JsonObjectType.String },
-                },
-            };
-            parameter.Schema.Enumeration.Add("en-US");
-            parameter.Schema.Enumeration.Add("fr-FR");
-            parameter.Schema.Enumeration.Add("ar-TN)");
-            context.OperationDescription.Operation.Parameters.Add(parameter);
-            return true;
+            });
+
         }
     }
 }
