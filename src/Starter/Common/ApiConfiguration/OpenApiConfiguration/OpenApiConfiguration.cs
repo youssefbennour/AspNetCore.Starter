a7035167965b2b77571a990a8615957415037afc
youssefbennour <youssefbennour15@gmail.com>
2024-04-26 11:51:19 +0100

    FEAT: add swagger configuration

---
+++ src/Starter/Common/ApiConfiguration/OpenApiConfiguration/OpenApiConfiguration.cs
@@ -0,0 +1,22 @@
+﻿
+using Swashbuckle.AspNetCore.Filters;
+
+namespace Starter.Common.ApiConfiguration.OpenApiConfiguration {
+    internal static class OpenApiConfiguration {
+        internal static IServiceCollection AddOpenApiConfiguration(this IServiceCollection services) {
+            services.AddSwaggerGen(c => {
+                c.OperationFilter<AcceptLanguageHeaderFilter>();
+            });
+
+            services.AddSwaggerExamplesFromAssemblyOf<Program>();
+            return services;
+        }
+
+        internal static IApplicationBuilder UseSwagger(this IApplicationBuilder app) {
+            SwaggerBuilderExtensions.UseSwagger(app);
+            app.UseSwaggerUI();
+
+            return app;
+        }
+    }
+}
