65c1d4e5e3dc5fc0736da0a42dda17a030b414af
youssefbennour <youssefbennour15@gmail.com>
2024-04-26 11:50:47 +0100

    FEAT: add api query models

---
+++ src/Starter/Common/QueryModels/QueryParameters.cs
@@ -0,0 +1,18 @@
+﻿namespace Starter.Common.QueryModels {
+    internal class QueryParameters {
+        private const int maxPageSize = 100;
+        internal int PageNumber { get; set; } = 1;
+        private int _pageSize = 10;
+
+        internal int PageSize {
+            get {
+                return _pageSize;
+            }
+            set {
+                _pageSize = value > maxPageSize ? maxPageSize : value;
+            }
+        }
+
+        internal string? searchTerm { get; set; }
+    }
+}
