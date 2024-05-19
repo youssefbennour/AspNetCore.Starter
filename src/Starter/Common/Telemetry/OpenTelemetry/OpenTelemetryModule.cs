89298f2fa0b24c6c0fed02880e616e809a41a621
youssefbennour <youssefbennour15@gmail.com>
2024-05-19 15:30:24 +0100

    REFACTOR: OpenTelemetry module, and include service names

--- src/Starter/Common/Telemetry/OpenTelemetry/OpenTelemetryModule.cs
+++ src/Starter/Common/Telemetry/OpenTelemetry/OpenTelemetryModule.cs
@@ -8,54 +8,77 @@ using OpenTelemetry.Trace;
 
 namespace Starter.Common.Telemetry.OpenTelemetry {
     internal static class OpenTelemetryModule {
-        internal static IServiceCollection AddOpenTelemetryModule(this WebApplicationBuilder builder) {
-            builder.Logging
-                .AddOpenTelemetry(options => {
-                    options.IncludeFormattedMessage = true;
-                    options.IncludeScopes = true;
-
-                    var resBuilder = ResourceBuilder.CreateDefault();
-                    var serviceName = builder.Configuration["ServiceName"]!;
-                    resBuilder.AddService(serviceName);
-                    options.SetResourceBuilder(resBuilder);
-
-                    options.AddOtlpExporter();
-                });
-
-            builder.Services.AddHttpLogging(o => o.LoggingFields = HttpLoggingFields.All);
-            builder.Services.Configure<AspNetCoreTraceInstrumentationOptions>(options => {
-                // Filter out instrumentation of the Prometheus scraping endpoint.
-                options.Filter = ctx => ctx.Request.Path != "/metrics";
-            });
+        internal static IServiceCollection AddOpenTelemetryModule(this WebApplicationBuilder builder)
+        {
+            string serviceName = GetServiceName(builder);
+            builder.AddOpenTelemetryLogging(serviceName);
 
             builder.Services.AddOpenTelemetry()
-                .AddTracing(builder.Configuration)
-                .AddMetrics(builder.Configuration);
+                .AddTracing(serviceName)
+                .AddMetrics(serviceName);
 
             return builder.Services;
         }
 
-        internal static IOpenTelemetryBuilder AddTracing(this IOpenTelemetryBuilder optlBuilder, IConfiguration configuration) {
-            optlBuilder.WithTracing(b => b
-                .AddAspNetCoreInstrumentation()
-                .AddHttpClientInstrumentation()
-                .AddEntityFrameworkCoreInstrumentation()
-                .AddOtlpExporter());
+        private static WebApplicationBuilder AddOpenTelemetryLogging(this WebApplicationBuilder builder, string serviceName)
+        {
+            builder.Logging.AddOpenTelemetry(options => {
+                options.IncludeFormattedMessage = true;
+                options.IncludeScopes = true;
+                options.SetResourceBuilder(ResourceBuilder
+                    .CreateDefault()
+                    .AddService(serviceName));
+                    
+                options.AddOtlpExporter();
+            });
+            
+            builder.Services.AddHttpLogging(o => o.LoggingFields = HttpLoggingFields.All);
+            builder.Services.Configure<AspNetCoreTraceInstrumentationOptions>(options =>
+            {
+                // Filter out instrumentation of the Prometheus scraping endpoint.
+                options.Filter = ctx => ctx.Request.Path != "/metrics";
+            });
+
+            return builder;
+        }
+        
+        private static IOpenTelemetryBuilder AddTracing(this IOpenTelemetryBuilder optlBuilder, string serviceName) {
+            optlBuilder.WithTracing(b =>
+            {
+                b.SetResourceBuilder(ResourceBuilder
+                    .CreateDefault()
+                    .AddService(serviceName));
+                
+                b.AddAspNetCoreInstrumentation()
+                    .AddHttpClientInstrumentation()
+                    .AddEntityFrameworkCoreInstrumentation()
+                    .AddOtlpExporter();
+            });
 
             return optlBuilder;
         }
-
-        internal static IOpenTelemetryBuilder AddMetrics(this IOpenTelemetryBuilder optlBuilder, IConfiguration configuration) {
-            optlBuilder.WithMetrics(b => b
-                .AddAspNetCoreInstrumentation()
-                .AddHttpClientInstrumentation()
-                .AddRuntimeInstrumentation()
-                .AddProcessInstrumentation()
-                .AddPrometheusExporter(o =>
-                o.DisableTotalNameSuffixForCounters = true));
+        
+        private static IOpenTelemetryBuilder AddMetrics(this IOpenTelemetryBuilder optlBuilder, string serviceName) {
+            optlBuilder.WithMetrics(b =>
+                {
+                    b.SetResourceBuilder(ResourceBuilder
+                        .CreateDefault()
+                        .AddService(serviceName));
+                    
+                     b.AddAspNetCoreInstrumentation()
+                     .AddHttpClientInstrumentation()
+                     .AddRuntimeInstrumentation()
+                     .AddProcessInstrumentation()
+                     .AddPrometheusExporter(o =>
+                         o.DisableTotalNameSuffixForCounters = true);
+                }
+                );
 
             return optlBuilder;
         }
+        
+        private static string GetServiceName(WebApplicationBuilder builder) => 
+            builder.Configuration["ServiceName"] ?? "unknown-service";
 
         internal static IApplicationBuilder UseOpenTelemetry(this IApplicationBuilder app) {
             return app.UseOpenTelemetryPrometheusScrapingEndpoint();
