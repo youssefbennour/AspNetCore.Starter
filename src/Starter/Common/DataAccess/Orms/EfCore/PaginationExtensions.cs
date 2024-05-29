1e277902b78a7e7e857c765fc3a33a037375f8a3
youssefbennour <youssefbennour15@gmail.com>
2024-05-29 22:06:09 +0100

    FEAT: introduce helper methods for dealing with pagination

---
+++ src/Starter/Common/DataAccess/Orms/EfCore/PaginationExtensions.cs
@@ -0,0 +1,48 @@
+using Starter.Common.Requests.Models;
+
+namespace Microsoft.Linq.Queryable;
+
+public static class PaginationExtensions
+{
+    private const int DefaultPageSize = 10;
+    private const int DefaultPageNumber = 1;
+
+    public static async Task<PaginatedList<T>> ToPaginatedListAsync<T>(
+        this IQueryable<T> query, 
+        QueryParameters queryParameters, 
+        CancellationToken cancellationToken = default (CancellationToken)) 
+        where T : class
+    {
+        var items = await query.Paginate(queryParameters)
+            .ToListAsync(cancellationToken);
+
+        var count = await query.CountAsync(cancellationToken);
+
+        return new PaginatedList<T>(items, queryParameters, count);
+    }
+   
+    public static async Task<PaginatedList<T>> ToPaginatedListAsync<T>(
+        this IQueryable<T> query, 
+        int pageNumber,
+        int pageSize,
+        CancellationToken cancellationToken = default (CancellationToken)) 
+        where T : class
+    {
+        var items = await query.Paginate(pageNumber, pageSize)
+            .ToListAsync(cancellationToken);
+
+        var count = await query.CountAsync(cancellationToken);
+
+        return new PaginatedList<T>(items, pageNumber, pageSize, count);
+    }
+    
+    public static IQueryable<T> Paginate<T>(this IQueryable<T> query, QueryParameters? requestParameters)
+        where T : class => 
+        query.Paginate(
+            pageNumber:requestParameters?.PageNumber ?? DefaultPageNumber,
+            pageSize: requestParameters?.PageSize ?? DefaultPageSize);
+        
+    public static IQueryable<T> Paginate<T>(this IQueryable<T> query, int pageNumber, int pageSize)
+        where T : class => query.Skip((pageNumber - 1) * pageSize)
+        .Take(pageSize);
+}
